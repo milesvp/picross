7e210cb0391822687d1e7cf8d58e59a00dfa56bf
milesvp <milesvp@83c5f06f-114d-0410-a33c-9ed400b1a7cd>
2014-02-25 07:56:56 +0000

    accepts mouse click. also gets col# based on x coord.
    git-svn-id: https://svn.picturecrossword.com/picross/trunk@209 83c5f06f-114d-0410-a33c-9ed400b1a7cd

--- canvas/picross.js
+++ canvas/picross.js
@@ -1,7 +1,7 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-var board_xoffset = 4;
-var board_yoffset = 4;
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var board_xoffset = 1;
+var board_yoffset = 1;
 var sq_width = 20;
 var sq_height = 20;
 ctx.lineWidth = 1;
@@ -11,18 +11,18 @@ var alt_stroke_color = "#000088";
 
 function CreateBoard(width, height) {
   var board = [];
-  for ( y=0; y<height; y++ ) {
+  for ( i=0; i<height; i++ ) {
     var row = [];
-    for ( x=0; x<width; x++ ) {
+    for ( j=0; j<width; j++ ) {
       row.push(0);
     }
     board.push(row);
   } 
   return board;
 }
-function DrawSquare(x,y, state) {
-  var xpixel = (x*sq_width)  + board_xoffset + (alt_line_width * (Math.floor(x/5)));
-  var ypixel = (y*sq_height) + board_yoffset + (alt_line_width * (Math.floor(y/5)));
+function DrawSquare(col,row, state) {
+  var xpixel = (col*sq_width)  + board_xoffset + (alt_line_width * (Math.floor(col/5)));
+  var ypixel = (row*sq_height) + board_yoffset + (alt_line_width * (Math.floor(row/5)));
   ctx.fillStyle = "#FFFFFF"; 
   if (state == 1) {
     ctx.fillStyle = "#FF0000"; 
@@ -32,10 +32,20 @@ function DrawSquare(x,y, state) {
   ctx.strokeRect(xpixel,ypixel,sq_width,sq_height);
 }
 
+function GetCol(x){
+  return col = Math.floor((x-board_xoffset+line_width) / (sq_width + (alt_line_width / 5)));
+}
+
+function GetColRow(x,y){
+  var col = GetCol(x);
+  var row = GetRow(y);
+  return col,row;
+}
+
 function DrawBoard(board){
-  for (x=0; x<board[0].length; x++){
-    for (y=0; y<board.length; y++){ 
-      DrawSquare(x,y,board[x][y]);
+  for (col=0; col<board[0].length; col++){
+    for (row=0; row<board.length; row++){ 
+      DrawSquare(col,row,board[row][col]);
     }
   }
   DrawAltLines(board);
@@ -45,6 +55,34 @@ function DrawAltLines(board) {
 
 }
 
-var board = CreateBoard(12,14);
+
+function getPosition(event)
+{
+  var x = 0;
+  var y = 0;
+  if (event.x != undefined && event.y != undefined)
+  {
+    x = event.x;
+    y = event.y;
+  }
+  else // Firefox method to get the position
+  {
+    x = event.clientX + document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+    y = event.clientY + document.body.scrollTop +
+        document.documentElement.scrollTop;
+  }
+
+  var canvas = document.getElementById("myCanvas");
+
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+
+  
+  alert("x:" + x + " y:" + y + " col:" + GetCol(x));
+} 
+
+var board = CreateBoard(25,14);
 board[3][2] = 1;
-DrawBoard(board)
+DrawBoard(board);
+canvas.addEventListener("mousedown", getPosition, false);
