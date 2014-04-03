51a87c7f673780a604c71b05179fd3e256d3dfd0
milesvp <milesvp@83c5f06f-114d-0410-a33c-9ed400b1a7cd>
2014-04-03 01:04:59 +0000

    got row clues showing up properly to the right of the board. various other bug fixes, and some code organization
    git-svn-id: https://svn.picturecrossword.com/picross/trunk@220 83c5f06f-114d-0410-a33c-9ed400b1a7cd

--- canvas/picross.js
+++ canvas/picross.js
@@ -1,59 +1,71 @@
-var board_xoffset    = 1;
-var board_yoffset    = 1;
-var sq_width         = 20;
-var sq_height        = 20;
-var line_width       = 1;
-var alt_line_width   = 2;
-var alt_stroke_color = "#000088";
-var text_color       = "#FFFFFF";
-var starting_color   = false;
-var overwrite_color  = false;
-var mouse_down       = false;
-var mouse_button     = false;
-var erase            = false;
-var last_cell_color  = false;
-var undo_stack       = [];
-var this_move        = [];
-var mouse_move_hash  = {};
-var starting_id      = false;
-var starting_square  = undefined;
-var second_square    = undefined;
-var to_state         = undefined;
-var second_id        = false;
-var last_id          = false;
-var left_button      = 0;
-var middle_button    = 1;
-var right_button     = 2;
-var empty_state      = 0;
-var on_state         = 1;
-var x_state          = 2;
-var dirty            = false;
+var board_xoffset     = 1;
+var board_yoffset     = 1;
+var sq_width          = 20;
+var sq_height         = 20;
+var board_font        = "italic 11pt Ariel";
+var line_width        = 1;
+var alt_line_width    = 2;
+var alt_stroke_color  = "#000088";
+var text_color        = "#000000";
+var starting_color    = false;
+var overwrite_color   = false;
+var mouse_down        = false;
+var mouse_button      = false;
+var erase             = false;
+var last_cell_color   = false;
+var undo_stack        = [];
+var this_move         = [];
+var mouse_move_hash   = {};
+var starting_id       = false;
+var starting_square   = undefined;
+var second_square     = undefined;
+var to_state          = undefined;
+var second_id         = false;
+var last_id           = false;
+var left_button       = 0;
+var middle_button     = 1;
+var right_button      = 2;
+var empty_state       = 0;
+var on_state          = 1;
+var x_state           = 2;
+var dirty             = false;
+var canvas_dimensions = undefined;
+var clue_row_spacer   = 5;
+var clue_col_spacer   = 5;
 
 function DrawLine(point, state) {
 
 }
 
-function DrawSquare(row,col, state) {
-  var xpixel = (row*sq_height) + board_yoffset + (alt_line_width * (Math.floor(row/5)));
-  var ypixel = (col*sq_width)  + board_xoffset + (alt_line_width * (Math.floor(col/5)));
-  ctx.fillStyle = "#FFFFFF"; 
+function DrawSquare(col, row, state) {
+  var xpixel = GetX(col);
+  var ypixel = GetY(row);
+  board_ctx.fillStyle = "#FFFFFF"; 
   if (state == 1) {
-    ctx.fillStyle = "#FF0000"; 
+    board_ctx.fillStyle = "#FF0000"; 
   }
   if (state == 2) {
-    ctx.fillStyle = "#888888";
+    board_ctx.fillStyle = "#888888";
   }
-  ctx.strokeStyle = "#0000FF";
-  ctx.fillRect(xpixel,ypixel,sq_width,sq_height);
-  ctx.strokeRect(xpixel,ypixel,sq_width,sq_height);
+  board_ctx.strokeStyle = "#0000FF";
+  board_ctx.fillRect(xpixel,ypixel,sq_width,sq_height);
+  board_ctx.strokeRect(xpixel,ypixel,sq_width,sq_height);
 }
 
 function GetCol(x){
-  return Math.floor((x-board_xoffset+line_width) / (sq_width + (alt_line_width / 5)));
+  return Math.floor((x-board_xoffset+line_width-canvas_dimensions.row_clues_width) / (sq_width + (alt_line_width / 5)));
 }
 
 function GetRow(y){
   return Math.floor((y-board_yoffset+line_width) / (sq_width + (alt_line_width / 5)));
+}
+
+function GetX(col){
+  return (col*sq_height) + board_xoffset + (alt_line_width * (Math.floor(col/5))) + canvas_dimensions.row_clues_width + clue_row_spacer;
+}
+
+function GetY(row){
+  return (row*sq_width)  + board_yoffset + (alt_line_width * (Math.floor(row/5)));
 }
 
 function GetColRow(point){
@@ -64,8 +76,15 @@ function GetColRow(point){
 }
 
 function DrawBoard(board){
-  for (row=0; row<board.length; row++){
-    for (col=0; col<board[0].length; col++){ 
+  var row_clues = CalcRowClues(board);
+  for (var i=0; i<row_clues.length; i++){ 
+    board_ctx.fillStyle = text_color;
+    board_ctx.font = '13px Ariel';//board_font;
+    board_ctx.textAlign = 'right';
+    board_ctx.fillText(row_clues[i], canvas_dimensions.row_clues_width, 16 + GetY(i));
+  }
+  for (var row=0; row<board.length; row++){
+    for (var col=0; col<board[0].length; col++){ 
       DrawSquare(row,col,board[row][col]);
     }
   }
@@ -112,8 +131,6 @@ function GetMouse(event) {
     y = event.clientY + document.body.scrollTop +
         document.documentElement.scrollTop;
   }
-
-  var canvas = document.getElementById("myCanvas");
 
   x -= canvas.offsetLeft;
   y -= canvas.offsetTop;
@@ -293,15 +310,42 @@ function ToggleState(state, button) {
   }
 }
 
-function LoadCanvas(id, size) {
+function Load3Canvas(id) {
   var canvas = document.createElement('canvas');
+  var row_canvas = document.createElement('canvas');
+  var col_canvas = document.createElement('canvas');
   div = document.getElementById(id); 
+  col_canvas.id     = "colCanvas";
+  col_canvas.width  = 200;
+  col_canvas.height = 50;
+  col_canvas.style.zIndex   = 8;
+  col_canvas.style.position = "absolute";
+  div.appendChild(col_canvas);
+  row_canvas.id     = "rowCanvas";
+  row_canvas.width  = 100;
+  row_canvas.height = 100;
+  row_canvas.style.zIndex   = 8;
+  row_canvas.style.position = "absolute";
+  div.appendChild(row_canvas);
   canvas.id     = "myCanvas";
-  canvas.width  = size.width;
-  canvas.height = size.height;
+  canvas.width  = 1;
+  canvas.height = 1;
   canvas.style.zIndex   = 8;
   canvas.style.position = "absolute";
   div.appendChild(canvas);
+  return canvas;
+}
+
+function LoadCanvas(id) {
+  var canvas = document.createElement('canvas');
+  div = document.getElementById(id); 
+  canvas.id     = "myCanvas";
+  canvas.width  = 1;
+  canvas.height = 1;
+  canvas.style.zIndex   = 8;
+  canvas.style.position = "absolute";
+  div.appendChild(canvas);
+  return canvas;
 }
 
 function CreateBoard(dimensions, chance) {
@@ -382,50 +426,62 @@ function CalcColClues(board) {
   return final_clues; 
 }
 
-function CalcRowCluesSize(board) {
-  //worst case alternating squares resulting n/2+1 clues
-  return height;
+function CalcRowCluesSize(clues) {
+  var longest_text_size = 0;
+  var text_size;
+  for (var i = 0; i < clues.length; i++) {
+    text_size = board_ctx.measureText(clues[i]);
+    if (text_size.width > longest_text_size) {
+      longest_text_size = text_size.width;
+    }
+  }
+  return longest_text_size;
 }
 
-function CalcColCluesSize(board) {
-  for (var row in board) {
-    continue;
-  }
+function CalcColCluesSize(clues) {
   return width;
 }
 
 function CalcCanvasSize(board) {
-  var pixel_width  = (board.cols*sq_height)  + (2 * board_xoffset)
-                     + (alt_line_width * (Math.floor(board.cols/5 )));
-  var pixel_height = (board.rows*sq_width)  + (2 * board_yoffset)
-                     + (alt_line_width * (Math.floor(board.rows/5)));
-  if (board.cols % 5 == 0) 
+  var pixel_width  = (board.length*sq_height)  + (2 * board_xoffset)
+                     + (alt_line_width * (Math.floor(board.length/5 )));
+  var pixel_height = (board[0].length*sq_width)  + (2 * board_yoffset)
+                     + (alt_line_width * (Math.floor(board[0].length/5)));
+  if (board.length % 5 == 0) 
     pixel_width -= alt_line_width;
-  if (board.rows % 5 == 0) 
+  if (board[0].length % 5 == 0) 
     pixel_height -= alt_line_height;
   
+  var row_clues_width = CalcRowCluesSize(CalcRowClues(board));
   return { width: pixel_width,
-           height: pixel_height }; 
+           height: pixel_height,
+           row_clues_width: row_clues_width }; 
 }
+
+function ResizeCanvas(dimensions) {
+  canvas.width = dimensions.width + dimensions.row_clues_width + clue_row_spacer;
+  canvas.height = dimensions.height;
+}
+
+
+var canvas = LoadCanvas('board');
+var board_ctx = canvas.getContext("2d");
+board_ctx.lineWidth = line_width;
+canvas.addEventListener("mousedown", HandleMouseDown, false);
+
+document.onmouseup = GlobalMouseUp;
+document.oncontextmenu = function () { return false; };
+var redraw_timer = setInterval(DrawChanges, 1000/20);
 
 
 var board_dimensions = { cols:6,
                          rows:4 };
 var board = CreateBoard(board_dimensions, 0);
 var solution = CreateBoard(board_dimensions, .5);
-var foo = CalcCanvasSize(board_dimensions);
-LoadCanvas('board', CalcCanvasSize(board_dimensions));
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.lineWidth = line_width;
+board_ctx.font = board_font;
+var canvas_dimensions = CalcCanvasSize(solution);
+ResizeCanvas(canvas_dimensions);
 DrawBoard(solution);
-canvas.addEventListener("mousedown", HandleMouseDown, false);
-ctx.fillStyle = text_color;
-ctx.font = "italic 11pt Ariel";
-ctx.fillStyle = "#000000";
-ctx.fillText('1 1 1', 15, 16);
-console.log(ctx.measureText('1 1 1'));
-document.onmouseup = GlobalMouseUp;
-document.oncontextmenu = function () { return false; };
-var redraw_timer = setInterval(DrawChanges, 1000/20);
+
+
 
